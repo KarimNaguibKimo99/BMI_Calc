7fec50e43797993c97a2efb4de4bf795e81434b1
KarimNaguibKimo99 <39785260+KarimNaguibKimo99@users.noreply.github.com>
2021-10-13 04:46:57 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:BMI_Calculator/screens/bmi_calcuator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BMICalcuator(),
+  ));
+}
